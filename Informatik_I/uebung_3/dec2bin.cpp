7beb1d597b6b6995cc57c92bb6ce98458a14d144
root <root@localhost.localdomain>
2017-10-13 12:21:56 +0200

    dec2bin update

--- Informatik_I/uebung_3/dec2bin.cpp
+++ Informatik_I/uebung_3/dec2bin.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main(void){
 
 	unsigned int i, r;
-
+	int c = 31;
+	vector<int> binary (32, 0);
+	
 	cout << "Bitte geben Sie ihre Dezimalzahl ein: ";
 	cin >> i;
 	do {
 		r = i % 2;
 		i = i / 2;
-		cout << r << endl;
-	} while(i != 0);
+		if(r == 1){
+			binary[c] = 1;
+		} else {
+			binary[c] = 0;
+		}
+		c--;
+	} while(i != 0 && c >= 0);
+	
+	c++;
+	while(c <= 31){
+		cout << binary[c];
+		c++;
+	}
 
 	return 0;
 }
