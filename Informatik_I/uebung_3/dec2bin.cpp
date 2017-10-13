9eefa6fb43a98226a8a238b7b4ee0734ac1cf03a
root <root@localhost.localdomain>
2017-10-13 11:42:20 +0200

    dec2bin

--- Informatik_I/uebung_3/dec2bin.cpp
+++ Informatik_I/uebung_3/dec2bin.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int main(void){
 
-	unsigned int input = 0;
+	unsigned int i, r;
 
 	cout << "Bitte geben Sie ihre Dezimalzahl ein: ";
-	cin >> input;
-
-
-
-
+	cin >> i;
+	do {
+		r = i % 2;
+		i = i / 2;
+		cout << r << endl;
+	} while(i != 0);
 
 	return 0;
 }
