0c5f21a48d150565525c5d15369dcccd3ca66434
crsystems <christopher.reinwardt@web.de>
2017-10-13 08:13:19 +0200

    Update fib_overflow.cpp

--- Informatik_I/uebung_3/fib_overflow.cpp
+++ Informatik_I/uebung_3/fib_overflow.cpp
@@ -1,3 +1,5 @@
+// UNCOMMENT THE NEXT LINE TO ENABLE THE TESTS WITHOUT SUBMITTING
+#include "tests.h"
 #define MAX 4294967295
 #include <iostream>
 
@@ -11,47 +13,50 @@ int main(void){
 	int num = 0;
 	cout << "Please enter the number of requested Fibonacci numbers: ";
 	cin >> m;
+	cout << "\n";
 
 	switch(m){
 
 		case 0: num = 0;
 			break;
 
-		case 1: cout << "---- 0\n";
+		case 1: cout << "0\n";
 			num = 1;
 			break;
 		default: 
-			cout << "---- 0\n";
-			cout << "---- 1\n";
+			cout << "0\n";
+			cout << "1\n";
 			num = fibonacci(m);
 			break;
 	}
 
-	cout << "Printed  " << num << " of " << m << " requested Fibonacci numbers" << endl;
+	cout << num << " of " << m << endl;
 	return 0;
 }
 
 int fibonacci(unsigned int top){
 	unsigned int i = 1, j = 1;
-	int count = 3;
+	unsigned int count = 3;
 	bool overflow = false;
 	while(count <= top && overflow == false){
 		if(i == 1 && j == 1){
-			cout << "---- 1\n";
+			cout << "1\n";
 			j = j + i;
 		}
 		else if(j > i){
-			cout << "---- " << j << endl;
+			cout << j << endl;
 			if((MAX-j) <= i){
 				overflow = true;
+				count++;
 				break;
 			}
 			i += j;
 		}
 		else if(i > j){
-			cout << "---- " << i << endl;
+			cout << i << endl;
 			if((MAX-i) <= j){
 				overflow = true;
+				count++;
 				break;
 			}
 			j += i;
