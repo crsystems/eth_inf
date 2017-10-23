4581cb3de70f144fa06f5abd14244d4420eacd31
root <root@localhost.localdomain>
2017-10-23 16:26:34 +0200

    encryption

---
+++ Informatik_I/uebung_5/parabola_encrypted.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(void){
+
+	double xjsdfkjsdhfkuh24q3asg = 0, xkdskjgljasr3nassfdsgj2 = 0, sr5sgiuodsbsdliy = 0, vdfhalasdhhsdfzhd35sd = 0.7, kkjdfuebasfjkjsadhkj = 0.00001;
+	std::cin >> x >> y;
+
+	x2 = x * x;
+	val += (double) 1.3 * x;
+	val += (double) 0.9 * x2;
+
+	if(val == y){
+		std::cout << "yes" << std::endl;
+	} else if(val > y){
+		if((val-y) < precision){ 
+			std::cout << "yes" << std::endl;
+		} else {
+			std::cout << "no" << std::endl;
+		}
+	} else if(val < y){
+		if((y-val) < precision){
+			std::cout << "yes" << std::endl;
+		} else {
+			std::cout << "no" << std::endl;
+		}
+	}
+	return 0;
+}
