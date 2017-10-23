ffb0e44c121377346e3d10026b317b3624ae1bd1
crsystems <christopher.reinwardt@web.de>
2017-10-23 16:31:12 +0200

    Update parabola_encrypted.cpp

--- Informatik_I/uebung_5/parabola_encrypted.cpp
+++ Informatik_I/uebung_5/parabola_encrypted.cpp
@@ -3,22 +3,22 @@
 int main(void){
 
 	double xjsdfkjsdhfkuh24q3asg = 0, xkdskjgljasr3nassfdsgj2 = 0, sr5sgiuodsbsdliy = 0, vdfhalasdhhsdfzhd35sd = 0.7, kkjdfuebasfjkjsadhkj = 0.00001;
-	std::cin >> x >> y;
+	std::cin >> xjsdfkjsdhfkuh24q3asg >> sr5sgiuodsbsdliy;
 
-	x2 = x * x;
-	val += (double) 1.3 * x;
-	val += (double) 0.9 * x2;
+	xkdskjgljasr3nassfdsgj2 = xjsdfkjsdhfkuh24q3asg * xjsdfkjsdhfkuh24q3asg;
+	vdfhalasdhhsdfzhd35sd += (double) 1.3 * xjsdfkjsdhfkuh24q3asg;
+	vdfhalasdhhsdfzhd35sd += (double) 0.9 * xkdskjgljasr3nassfdsgj2;
 
-	if(val == y){
+	if(vdfhalasdhhsdfzhd35sd == sr5sgiuodsbsdliy){
 		std::cout << "yes" << std::endl;
-	} else if(val > y){
-		if((val-y) < precision){ 
+	} else if(vdfhalasdhhsdfzhd35sd > sr5sgiuodsbsdliy){
+		if((vdfhalasdhhsdfzhd35sd-sr5sgiuodsbsdliy) < kkjdfuebasfjkjsadhkj){ 
 			std::cout << "yes" << std::endl;
 		} else {
 			std::cout << "no" << std::endl;
 		}
-	} else if(val < y){
-		if((y-val) < precision){
+	} else if(vdfhalasdhhsdfzhd35sd < sr5sgiuodsbsdliy){
+		if((y-vdfhalasdhhsdfzhd35sd) < kkjdfuebasfjkjsadhkj){
 			std::cout << "yes" << std::endl;
 		} else {
 			std::cout << "no" << std::endl;
