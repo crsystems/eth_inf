0b8eb1d226af0493332a03c651684ec4ab8665f6
root <root@localhost.localdomain>
2017-11-04 23:56:15 +0100

    run_length

--- Informatik_I/uebung_6/run_length_cr.cpp
+++ Informatik_I/uebung_6/run_length_cr.cpp
@@ -1,50 +1,86 @@
-//Author: Christopher Reinwardt
-//Owner:  Christopher Reinwardt
-//
 #include <iostream>
 
 using namespace std;
 
-void encode(int *data){
-	int last_byte, cur_byte, count = 0;
-	int *swap;
+void encode(){
+	int cur_byte, last_byte, count = 0;
 
-	data = (int *) calloc(2, sizeof(int));
+	cout << "1 ";
 
-
-	while(...){
-
-		cin >> cur_byte;
-		last_byte = cur_byte;
+	cin >> cur_byte;
+	last_byte = cur_byte;
+	while(cur_byte != -1){
 		while(cur_byte == last_byte){
+			if(cur_byte > 255 || cur_byte < 0){
+				cout << "\rerror";
+			}
+			if(count == 255){
+				cout << count << " " << cur_byte << " ";
+				count = 0;
+			}
 			count++;
 			cin >> cur_byte;
 		}
-		swap = (int *) calloc(1, sizeof(data)+(2*sizeof(int)));
-		memcpy(swap, data, sizeof(data));
-		free(data);
-		data = swap;
+
+		cout << count << " " << last_byte << " ";
+		
+		last_byte = cur_byte;
+		count = 0;
+	}
+	cout << "-1";
+	return;
+
+}
+
+void decode(){
+	int byte = 0, count, counter = 0;
+
+	cout << "0 ";
+
+	while(byte != -1){
+		cin >> count;
+	        if(count == -1){
+			break;
+		}else if(count < 0 || count > 255){
+			cout << "\rerror";
+			break;
+		}
+		
+		cin >> byte;
+		if(byte < 0 || byte > 255){
+			cout << "\rerror";
+			break;
+		}else{
+			while(counter < count){
+				cout << byte << " ";
+				counter++;
+			}
+			counter = 0;
+		}
+	}
+	return;
+}
 
 
-
-
-
-
+void decide(int in){
+	switch(in){
+		case 0: encode();
+			break;
+		case 1: decode();
+			break;
+		default: cout << "error\n";
+			 break;
+	}
+	return;
+}
 
 
 int main(void){
-	
-	int *data;
 
-	cin >> input;
-	switch(input){
-		case 0: encode(data);
-			break;
-		case 1: decode(data);
-			break;
-		default: cout << "error";
-			 break;
-	}
+	int start;
+	cin >> start;
 
-	return 0;	
+	decide(start);
+
+	return 0;
 }
