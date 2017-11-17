969d453e913545dbe07d0d95ae9ee17163ab9f8f
Vyae <33265412+Vyae@users.noreply.github.com>
2017-11-17 21:10:05 +0100

    Create nzz_decoder_nn.cpp

---
+++ Informatik_I/uebung_8/nzz_decoder_nn.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+int main(){
+    
+    int byte, dec=0, remainder=0;
+    
+    std::string filename;
+    std::cin >> filename;
+    std::ifstream file (filename.c_str());
+    
+    while(file >> byte ){
+    
+        for(int i=0;i<=7;i++){
+            remainder = byte%10;
+            byte /= 10;
+            dec += remainder*pow(2,i);
+        }
+        std::cout << (char)dec;
+        dec=0; 
+        remainder=0;
+    }
+
+}
