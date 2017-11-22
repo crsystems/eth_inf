6f15a921e164c715a8adab94c18491811b16f412
Vyae <33265412+Vyae@users.noreply.github.com>
2017-11-22 21:03:37 +0100

    Create lexicographic_nn.cpp

---
+++ Informatik_I/uebung_9/lexicographic_nn.cpp
@@ -0,0 +1,41 @@
+#include "tests.h"
+
+#include <iostream>
+
+typedef std::string::iterator It;
+
+bool lexicographic_compare(It str1_b, It str1_e, It str2_b, It str2_e){
+    
+    for(int i=0; i<(str1_e-str1_b)||i<(str2_e-str2_b); i++){
+        
+        char value1=*(str1_b+i);
+        char value2=*(str2_b+i);
+        if (value1<value2){
+            
+            return true;
+            break;
+            
+        }else if(value1>value2){
+            
+            return false;
+            break;
+        }
+    }
+    
+}
+
+int main() {
+  // string input
+  std::string name1;
+  std::string name2;
+  std::cin >> name1;
+  std::cin >> name2;
+
+  //output result for standard library strings
+  if (lexicographic_compare (name1.begin(), name1.end(), name2.begin(), name2.end()))
+    std::cout << name1 << " < " << name2 << "\n";
+  else
+    std::cout << name1 << " >= " << name2 << "\n";
+
+  return 0;
+}
