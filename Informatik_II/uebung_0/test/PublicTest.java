fdd8ab86f2433a1bc813b2f04866d47757637283
Christopher Reinwardt <chris@XPS-Arch.localdomain>
2018-02-21 08:51:36 +0100

    Added Informatik_II directory

---
+++ Informatik_II/uebung_0/test/PublicTest.java
@@ -0,0 +1,20 @@
+/**
+ * Public tests.
+ * 
+ * All tests in the folder "test" are executed 
+ * when the "Test" action is invoked.
+ * 
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class PublicTest {
+
+    @Test
+    public final void test1() {
+        // Empty test passes.
+    }
+}
+
