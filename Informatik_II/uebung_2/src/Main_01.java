bc424eaefb95082e85a41e77cb0ac6ea174bcf7f
Christopher Reinwardt <christopher.reinwardt@web.de>
2018-03-10 02:33:05 +0100

    Task_01 finished and 50% of task_02

---
+++ Informatik_II/uebung_2/src/Main_01.java
@@ -0,0 +1,18 @@
+/**
+ * Main class of the Java program. 
+ * 
+ */
+
+public class Main_01 {
+
+    public static void main(String[] args) {
+        
+        // we print a heading and make it bigger using HTML formatting
+        System.out.println("<h4>-- RandomArray.java --</h4>");
+	RandomArray rand = new RandomArray(3);
+	System.out.println(rand.toString());
+	rand.sort();
+	System.out.println(rand.toString());
+    }
+}
+
---
+++ Informatik_II/uebung_2/src/RandomArray.java
@@ -0,0 +1,80 @@
+/**
+ * An array with random values.
+ */
+
+import java.util.Random;
+public class RandomArray {
+	/**
+	 * internal storage of the numbers
+	 */
+    private  int numbers[];
+
+    /**
+     * Create a new array with random values between 0 (inclusive) and 1000 (exclusive).
+     * 
+     * @param length the length of the new array.
+     */
+    public RandomArray(int length){
+	Random rand = new Random();
+
+	numbers = new int[length];
+	for(int i = 0; i < length; i++){
+		numbers[i] = rand.nextInt(100);
+	}
+    }
+
+    /**
+     * Return string-representation of array.
+     * 
+     * Example: the string-representation of int array[] = {1,2,3} is '[1, 2, 3]'
+     * 
+     * @return a list of the values, separated by a comma and a space, and enclosed in squared brackets.
+     */
+    public String toString(){
+	    String str_arr = new String("[");
+	    
+	    for(int i = 0; i < numbers.length; i++){
+		    str_arr = str_arr + numbers[i];
+		    if(i < (numbers.length - 1)){
+			    str_arr = str_arr + ", ";
+		    }
+	    }
+	    str_arr = str_arr + "]";
+	    return str_arr;
+    }
+
+    /**
+     * recursive sort of the array in descending order in place.
+     * 
+     * @param until value between 0 and length (inclusive) of {@link RandomArray#numbers}. <br>
+     * When the function returns the <em>until</em> largest values of {@link RandomArray#numbers} 
+     * are placed in the first <em>until</em> positions of the array and sorted in descending order.
+     */
+    private void recursiveSort(int until){
+	    if(until <= 0){
+		    return;
+	    }else if(until > 1){
+		    recursiveSort(until - 1);
+	    }
+
+	    int max_index = until-1;
+	    for(int i = until; i < numbers.length; i++){
+		    if(numbers[i] > numbers[until-1]){
+			    max_index = i;
+		    }
+	    }
+
+	    int swap = numbers[max_index];
+	    numbers[max_index] = numbers[until-1];
+	    numbers[until-1] = swap;
+    }
+
+    /**
+     * sort the array in descending order in place.
+     */
+    public void sort()
+    {
+    	recursiveSort(numbers.length);        
+    }
+}
+
