f48509cf91900d7d368c09fd2fea2dab472125b8
Christopher Reinwardt <christopher.reinwardt@web.de>
2018-04-05 22:06:47 +0200

    Finished task 3

--- Informatik_II/bonus_uebung_1/src/Main.java
+++ Informatik_II/bonus_uebung_1/src/Main.java
@@ -5,29 +5,41 @@
 
 public class Main {
 
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
         
-        /**
+        
+	/**
          * Lesen Sie "./Root/files/echo.csv" und "./Root/files/codeexpert.csv" ein
          * und geben Sie die entsprechenden Warnung auf System.out aus
          */
 
+
+	  
+	   //String arrays for the echo and codeexpert csv files
 	   String[][] echo = new CSVReader().readCSV("../files/echo.csv");
 	   String[][] codeexpert = new CSVReader().readCSV("../files/codeexpert.csv"); 
 
+	   //Array of Student objects
 	   Student[] students = new Student[echo.length + codeexpert.length];
 
-	   
-
+	   //Sorting the echo array from small to big LEGI
 	   for(int i = 0; i < echo.length; i++){
 
+		  //generating dash free LEGI string the ugly way...
 		  String source_legi = "" + echo[i][0].charAt(0) + echo[i][0].charAt(1) + echo[i][0].charAt(3) + echo[i][0].charAt(4) + echo[i][0].charAt(5) + echo[i][0].charAt(7) + echo[i][0].charAt(8) + echo[i][0].charAt(9);
 
+		  //... and generating an integer from that
 		  int src = Integer.parseInt(source_legi);
+
+		  //best found legi is a priori the current one
 		  int best = src;
 		  int best_index = i;
 
+		  //cycling through the unsorted part of the array to find a student with smaller LEGI
 		  for(int k = i+1; k < echo.length; k++){
+
+			//the ugly thing again
 			String new_legi = "" + echo[k][0].charAt(0) + echo[k][0].charAt(1) + echo[k][0].charAt(3) + echo[k][0].charAt(4) + echo[k][0].charAt(5) + echo[k][0].charAt(7) + echo[k][0].charAt(8) + echo[k][0].charAt(9);
 			
 			int n_legi = Integer.parseInt(new_legi);
@@ -38,6 +50,7 @@ public class Main {
 			}
 		  }
 
+		  //only swap the entrys if necessary (might save some time... Probrably not)
 		  if(best != src){
 			  String[] swap = echo[best_index];
 			  echo[best_index] = echo[i];
@@ -46,6 +59,8 @@ public class Main {
 
 	   }
 	   
+	   //Sorting the codeexpert array
+	   //Exactly the same loop as above, just echo was replaced by codeexpert
 	   for(int i = 0; i < codeexpert.length; i++){
 
 		  String source_legi = "" + codeexpert[i][0].charAt(0) + codeexpert[i][0].charAt(1) + codeexpert[i][0].charAt(3) + codeexpert[i][0].charAt(4) + codeexpert[i][0].charAt(5) + codeexpert[i][0].charAt(7) + codeexpert[i][0].charAt(8) + codeexpert[i][0].charAt(9);
@@ -73,38 +88,143 @@ public class Main {
 
 	   }
 
+
+	   //Filling the upper part of the student array with the students from echo...
 	   for(int i = 0; i < echo.length; i++){
 		   students[i] = new Student(echo[i][2], echo[i][1], echo[i][0], echo[i][3]);
 	   }
 
+	   //... and the remaining part with the students from codeexpert
 	   for(int i = echo.length; i < (echo.length + codeexpert.length); i++){
 		   students[i] = new Student(codeexpert[i - echo.length][2], codeexpert[i - echo.length][1], codeexpert[i - echo.length][0], codeexpert[i - echo.length][3]);
 	   }
 
 
-	   int err_size = codeexpert.length;
+	   //Searching for errors and setting the corresponding errorcodes in the student objects
+	   for(int i = 0; i < students.length; i++){
+		  
+		  //Splitting the search between the two sections of the student array
+		  if(i < echo.length){
 
-	   if(echo.length > codeexpert.length){
-		   err_size = echo.length;
+			//helper variable to find out if this student exists in the codeexpert array
+			boolean found = false;
+			for(int k = 0; k < codeexpert.length; k++){
+				//matching LEGIs => good sign, but lets check the first- and lastname to
+				if(students[i].getLEGI().equals(codeexpert[k][0])){
+
+					if(students[i].getFirstname().equals(codeexpert[k][2]) && students[i].getLastname().equals(codeexpert[k][1])){
+						//set helper variable
+						found = true;
+
+
+						//Decide which error is present
+						if(students[i].getTutor().equals(codeexpert[k][3])){
+							students[i].setError(0);
+						}else{
+							students[i].setError(1);
+						}
+						//Don't waste time. We already found one. That has to do the trick
+						break;
+					}
+				}
+			}
+
+			//if no equal student was found set the corresponding error
+			if(!found){
+				students[i].setError(3);
+			}
+
+		  //Same algorithm as above
+		  }else{
+			
+			boolean found = false;
+			for(int k = 0; k < echo.length; k++){
+				if(students[i].getLEGI().equals(echo[k][0])){
+					if(students[i].getFirstname().equals(echo[k][2]) && students[i].getLastname().equals(echo[k][1])){
+
+						found = true;
+
+						if(students[i].getTutor().equals(echo[k][3])){
+							students[i].setError(0);
+						}else{
+							students[i].setError(1);
+						}
+						break;
+					}
+				}
+			}
+
+			if(!found){
+				students[i].setError(2);
+			}
+
+		  }
+	   }
+	
+
+	   //Sorting the student array to be able to print the errors with ascending LEGI numbers later
+	   //Again it's the same sorting algorithm. But the ugly LEGI generation is in the Student class
+	   for(int i = 0; i < students.length; i++){
+		  int src = students[i].getLEGINum();
+		  int best = src;
+		  int best_index = i;
+
+		  for(int k = i+1; k < students.length; k++){
+			int n_legi = students[k].getLEGINum(); 
+
+			if(n_legi < src && n_legi < best){
+				best = n_legi;
+				best_index = k;
+			}
+		  }
+
+		  if(best != src){
+			  Student swap = students[best_index];
+			  students[best_index] = students[i];
+			  students[i] = swap;
+		  }
+
 	   }
 
-	   Student errors = new Student[err_size];
 
-	   for(int i = 0; i < echo.length; i++){
-		   for(int k = 0; k < codeexpert.length; k++){
-			   if(echo[i][0].equals(codeexpert[k][0])){
-				if(echo[i][3].equals(codeexpert[k][3]) == False){
-					errors[err_index].set
+	  //Iterating through the student arra to print the earlier found errors
+	  for(int i = 0; i < students.length; i++){
+		
+		  //If an error exists and this is not something, that already was printed
+		  if(students[i].getError() != 0 && students[i].getPrinted() == false){
+
+			  //Different tutors
+			  if(students[i].getError() == 1){
+				String echo_string = students[i].getTutor();
 				
-			   }
+				int code_index = i;
+				
+				//Find tutor of codeexpert
+				//We can be sure to search in the codeexpert part, because we have the printed variable
+				//If errorcode 1 is correct this if is executed in an echo index space
+				for(int k = i+1 ; k < students.length; k++){
+					if(students[i].getLEGI().equals(students[k].getLEGI())){
+						code_index = k;
+						students[k].setPrinted();
+						break;
+					}
+				}
 
+				System.out.println(students[i].getFirstname() + " " + students[i].getLastname() + " is enrolled to " + students[i].getTutor() + " in Echo, but registered with " + students[code_index].getTutor() + " in CodeExpert.");
 
+				students[i].setPrinted();
 
+			  //Not in Echo
+			  }else if(students[i].getError() == 2){
+				  System.out.println(students[i].getFirstname() + " " + students[i].getLastname() + " is in CodeExpert but not in Echo.");
+				  students[i].setPrinted();
 
-
-
-
-
-
+		  	  //Not in CodeExpert
+			  }else if(students[i].getError() == 3){
+				  System.out.println(students[i].getFirstname() + " " + students[i].getLastname() + " is in Echo but not in CodeExpert.");
+				  students[i].setPrinted();
+			  }
+		  }
+	  }
     }
 }
--- Informatik_II/bonus_uebung_1/src/Student.java
+++ Informatik_II/bonus_uebung_1/src/Student.java
@@ -2,26 +2,115 @@ public class Student{
 
 	String firstname, lastname, legi, tutor;
 
-	int legi_1, legi_2, legi_3;
+	/**
+	 *Error Variable
+	 *
+	 *Stores the kind of error, if any:
+	 *
+	 * 0 = No error
+	 * 1 = Different assistants in CodeExpert and Echo
+	 * 2 = Not in Echo
+	 * 3 = Not in CodeExpert
+	 * -1 = Default unclassified
+	 *
+	 */
+	int error;
 
-	public Student(){}
+	int legi_numeric;
 
+	boolean printed = false;
+
+	/**
+	 * Standard constructor without arguments
+	 */
+	public Student(){
+		this.error = -1;
+	}
+	
+	/**
+	 * Constructor that sets the atrributes of the object corresponding to the arguments
+	 */
 	public Student(String firstname, String lastname, String legi, String tutor){
 		this.set(firstname, lastname, legi, tutor);
 	}
 
+	/**
+	 * Setter method to setup the attributes of the object
+	 */
 	public void set(String firstname, String lastname, String legi, String tutor){
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.legi = legi;
 		this.tutor = tutor;
+		this.error = -1;
+
+		String source = "" + legi.charAt(0) + legi.charAt(1) + legi.charAt(3) + legi.charAt(4) + legi.charAt(5) + legi.charAt(7) + legi.charAt(8) + legi.charAt(9);
+
+                this.legi_numeric = Integer.parseInt(source);
 	}
 
+
+	/**
+	 * Method that returns a meaningful sentence including the attributes of the given object
+	 */
 	public String toString(){
-		if(tutor == "none"){
-			return this.firstname + " " + this.lastname + " with LEGI: " + this.legi + " is enrolled in the course of no one.";
-		}else{
-			return this.firstname + " " + this.lastname + " with LEGI: " + this.legi + " is enrolled in the course of " + this.tutor;
-		}
+		return this.firstname + " " + this.lastname + " with LEGI: " + this.legi + " is enrolled in the course of " + this.tutor;
+	}
+
+	/**
+	 * Method to set the errorcode of the object
+	 */
+	public void setError(int err){
+		this.error = err;
+	}
+
+	/**
+	 * Returns the errorcode of the object
+	 */
+	public int getError(){
+		return this.error;
 	}	
+	
+	/**
+	 * Returns the LEGI as string
+	 */
+	public String getLEGI(){
+		return this.legi;
+	}
+	
+	/**
+	 * Returns the LEGI in integer representation
+	 */
+	public int getLEGINum(){
+		return legi_numeric;
+	}
+
+	/**
+	 * Returns the Tutor
+	 */
+	public String getTutor(){
+		return this.tutor;
+	}
+
+	public void setPrinted(){
+		printed = true;
+	}
+
+	public boolean getPrinted(){
+		return printed;
+	}
+
+	/**
+	 * Returns the firstname
+	 */
+	public String getFirstname(){
+		return firstname;
+	}
+
+	/**
+	 * Returns the lastname
+	 */
+	public String getLastname(){
+		return lastname;
+	}
 }
